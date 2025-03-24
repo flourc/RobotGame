834131ca84ab723acd12811289ea80c3b731aa39
cyamam01 <charlotte.yamamoto@tufts.edu>
2025-03-24 18:56:41 -0400

    Main menu
    added functionality to pause menu, main menu

---
+++ robotgame/Assets/Scripts/GameHandler.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameHandler : MonoBehaviour
+{
+    private GameObject player;
+    private string sceneName;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player");
+        sceneName = SceneManager.GetActiveScene().name;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartGame() 
+    {
+        SceneManager.LoadScene("Level0");
+    }
+
+    public void MainMenu() 
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("MainMenu");
+        // Reset all static variables here, for new games:
+    }
+
+    public void QuitGame() 
+    {
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+        #else
+            Application.Quit();
+        #endif
+    }
+
+    public void Credits() 
+    {
+        SceneManager.LoadScene("Credits");
+    }
+}
