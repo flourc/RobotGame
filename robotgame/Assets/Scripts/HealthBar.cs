f277fe9f6e0faccc3ff1ef9ccc17d33482795e5b
cyamam01 <charlotte.yamamoto@tufts.edu>
2025-03-29 20:24:06 -0400

    Health bar UI component base

---
+++ robotgame/Assets/Scripts/HealthBar.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    public GameObject entity;
+
+    public int health;
+    public int startingHealth;
+    public float barFill;
+    public GameObject healthBar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startingHealth = 10;
+        barFill = 1.0f;
+        health = startingHealth;
+        healthBar.SetActive(true);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Minus)) {
+            GetHit(1);
+        }
+    }
+
+    public void GetHit(int dmg)
+    {
+        health -= dmg;
+        if (health < 0) {
+            health = 0;
+        }
+        barFill = (float)health / startingHealth;
+        updateStatsDisplay();
+    }
+
+    public void updateStatsDisplay()
+    {
+        healthBar.GetComponent<Image>().fillAmount = barFill;
+    }
+
+}
