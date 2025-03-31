90575e4563aaf96aee98b1f3c19abc567e0e82eb
JaylenCallender <jaylencallender@yahoo.com>
2025-03-31 16:10:23 -0400

    Made a prefab for the player which is called "player" found in the prefab folder. Started the tutorial level.

--- robotgame/Assets/Scripts/ThirdPersonCam.cs
+++ robotgame/Assets/Scripts/ThirdPersonCam.cs
@@ -10,22 +10,11 @@ public class ThirdPersonCam : MonoBehaviour
     public Transform playerObj;
     public CharacterController characterController; // Changed from Rigidbody to CharacterController
     public float rotationSpeed;
-    public Transform combatLookAt;
     public GameObject thirdPersonCam;
-    public GameObject combatCam;
-    public GameObject topDownCam;
-    public CameraStyle currentStyle;
     
     [Header("Cursor Settings")]
     [SerializeField] private KeyCode cursorToggleKey = KeyCode.Escape; // Key to toggle cursor
     private bool isCursorLocked = true;
-    
-    public enum CameraStyle
-    {
-        Basic,
-        Combat,
-        Topdown
-    }
     
     private void Start()
     {
@@ -51,44 +40,17 @@ public class ThirdPersonCam : MonoBehaviour
             ToggleCursorLock();
         }
         
-        // Switch camera styles
-        if (Input.GetKeyDown(KeyCode.Alpha1)) SwitchCameraStyle(CameraStyle.Basic);
-        if (Input.GetKeyDown(KeyCode.Alpha2)) SwitchCameraStyle(CameraStyle.Combat);
-        if (Input.GetKeyDown(KeyCode.Alpha3)) SwitchCameraStyle(CameraStyle.Topdown);
-        
         // Rotate orientation
         Vector3 viewDir = player.position - new Vector3(transform.position.x, player.position.y, transform.position.z);
         orientation.forward = viewDir.normalized;
         
         // Rotate player object
-        if (currentStyle == CameraStyle.Basic || currentStyle == CameraStyle.Topdown)
-        {
-            float horizontalInput = Input.GetAxis("Horizontal");
-            float verticalInput = Input.GetAxis("Vertical");
-            Vector3 inputDir = orientation.forward * verticalInput + orientation.right * horizontalInput;
-            
-            if (inputDir != Vector3.zero)
-                playerObj.forward = Vector3.Slerp(playerObj.forward, inputDir.normalized, Time.deltaTime * rotationSpeed);
-        }
-        else if (currentStyle == CameraStyle.Combat)
-        {
-            Vector3 dirToCombatLookAt = combatLookAt.position - new Vector3(transform.position.x, combatLookAt.position.y, transform.position.z);
-            orientation.forward = dirToCombatLookAt.normalized;
-            playerObj.forward = dirToCombatLookAt.normalized;
-        }
-    }
-    
-    private void SwitchCameraStyle(CameraStyle newStyle)
-    {
-        combatCam.SetActive(false);
-        thirdPersonCam.SetActive(false);
-        topDownCam.SetActive(false);
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        Vector3 inputDir = orientation.forward * verticalInput + orientation.right * horizontalInput;
         
-        if (newStyle == CameraStyle.Basic) thirdPersonCam.SetActive(true);
-        if (newStyle == CameraStyle.Combat) combatCam.SetActive(true);
-        if (newStyle == CameraStyle.Topdown) topDownCam.SetActive(true);
-        
-        currentStyle = newStyle;
+        if (inputDir != Vector3.zero)
+            playerObj.forward = Vector3.Slerp(playerObj.forward, inputDir.normalized, Time.deltaTime * rotationSpeed);
     }
     
     // Cursor Control Methods
