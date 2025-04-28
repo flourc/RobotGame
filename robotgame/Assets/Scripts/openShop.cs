8665d73d64e49550da915257f4e791403ec555f2
JaylenCallender <jaylencallender@yahoo.com>
2025-04-28 11:35:29 -0400

    Working on shop

---
+++ robotgame/Assets/Scripts/openShop.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class DoorNextScene : MonoBehaviour
+{
+    public string nextScene;
+    public float interactRadius;
+    public bool locked = true;
+    private Transform playerLoc;
+    public bool inRadius;
+    public Material activeMaterial;
+    public Material passiveMaterial;
+    private Renderer myRenderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRenderer = GetComponentInChildren<MeshRenderer>();
+        playerLoc = GameObject.FindWithTag("Player").GetComponent<Transform>();
+        inRadius = false;
+        myRenderer.material = passiveMaterial;
+        interactRadius = 5;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float dist = Vector3.Distance(playerLoc.position, transform.position);
+        inRadius = dist <= interactRadius;
+        if (inRadius && !locked) {
+            myRenderer.material = activeMaterial;
+        } else {
+            myRenderer.material = passiveMaterial;
+        }
+    }
+
+    public void GoNextScene()
+    {
+        SceneManager.LoadScene(nextScene);
+    }
+
+    public void Unlock()
+    {
+        locked = false;
+    }
+
+}
