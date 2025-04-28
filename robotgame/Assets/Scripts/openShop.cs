9722255ffe11a62fbfc523a8c03ba42ddb8d69fc
cyamam01 <charlotte.yamamoto@tufts.edu>
2025-04-28 17:53:21 -0400

    more UI elements

--- robotgame/Assets/Scripts/openShop.cs
+++ robotgame/Assets/Scripts/openShop.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 
-public class DoorNextScene : MonoBehaviour
+public class OpenShop : MonoBehaviour
 {
     public string nextScene;
     public float interactRadius;
