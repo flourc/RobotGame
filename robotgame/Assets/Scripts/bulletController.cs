4ef9130564e1feffff48b92235591f1540cef633
Ivi <60819778+MostEsteemedFungus@users.noreply.github.com>
2025-05-05 11:22:06 -0400

    destroy bullets that go too far

--- robotgame/Assets/Scripts/bulletController.cs
+++ robotgame/Assets/Scripts/bulletController.cs
@@ -7,26 +7,32 @@ public class bulletController : MonoBehaviour
     public int damage = 1;
     public GameObject handler;
     private HealthBar playerHP;
+    private Vector3 startPosition;
+    public float maxDistance = 30f;
 
     // Start is called before the first frame update
     void Start()
     {
         handler = GameObject.FindWithTag("GameController");
         playerHP = handler.GetComponent<HealthBar>();
+        startPosition = transform.position;
         //Debug.Log("Bullet tag at start: " + gameObject.tag);
     }
 
     // Update is called once per frame
     void Update()
     {
-  
+        float distanceTravelled = Vector3.Distance(startPosition, transform.position);
+        if (distanceTravelled > maxDistance) {
+            Destroy(gameObject); // Bullet has traveled too far, destroy it
+        }
     }
 
     private void OnCollisionEnter (Collision collider) 
     {
 
         if (this.gameObject.CompareTag("weapon")) {
-            Debug.Log("player bullet hit!");
+            //Debug.Log("player bullet hit!");
             if (collider.gameObject.tag != "Player") {
                 if (collider.gameObject.GetComponent<EntityHealth>() != null) {
                     collider.gameObject.GetComponent<EntityHealth>().TakeDamage(damage);
@@ -37,7 +43,7 @@ public class bulletController : MonoBehaviour
             
         }
         else if (this.gameObject.CompareTag("weapon_enemy")) {
-            Debug.Log("enemy bullet hit!");
+            //Debug.Log("enemy bullet hit!");
             if (collider.gameObject.tag != "enemy") {
                 if (collider.gameObject.GetComponent<EntityHealth>() != null) {
                 collider.gameObject.GetComponent<EntityHealth>().TakeDamage(damage);
