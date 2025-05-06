453a8ebcd29214dfc6d8d5ff729113457a79f0ee
cyamam01 <charlotte.yamamoto@tufts.edu>
2025-05-06 16:48:54 -0400

    post-build

---
+++ robotgame/Assets/Scripts/GameHandler_scripts/EndWinHandler.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndWinHandler : MonoBehaviour
+{
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.None;
+    }
+
+    void Update()
+    {
+        
+    }
+}
